eb1d108017eb733fbcdf0a24444f73a298fcedc0
cooligus <pewienjaaa@gmail.com>
2023-09-15 11:12:49 +0200

    Finish all required tasks

--- src/Zadanie3.java
+++ src/Zadanie3.java
@@ -1,16 +1,26 @@
 public class Zadanie3 {
+    // Be aware! This is not foolproof method!
     public static void main(String[] args) {
-        // TODO: do it with brain
-//        *
-//***
-//*****
-//*******
-//*****
-//***
-//*
+        int howManyLines = 7;
+        int initialStars = 1;
+        int starsToMulitpleBy = 2;
+        for (int i = 0; i < howManyLines; i++) {
+            int howManyStars = i * starsToMulitpleBy + initialStars;
+            if(howManyStars > howManyLines){
+                howManyStars = (howManyLines - i - 1) * starsToMulitpleBy + initialStars;
+            }
+            int howManySpaces = (howManyLines - howManyStars) / 2;
 
-        for (int i = 0; i < 7; i++) {
-            System.out.println();
+            for (int j = 0; j < howManySpaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < howManyStars; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < howManySpaces; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("\n");
         }
 
     }
